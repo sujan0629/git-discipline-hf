039c78061963c1eb5cea6e052576d0335032e481
sujan0629 <sujanbhatta0629@gmail.com>
2026-02-14 20:52:41 +0545

    feat: conversion to template 2nd

--- .history/tests/smoke.test_20260214173833.js
+++
@@ -1,7 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { sum } from '../src/index.js';
-
-test('sum adds two numbers', () => {
-  assert.equal(sum(2, 3), 5);
-});
